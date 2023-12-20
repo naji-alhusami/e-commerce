1586c5557a293a233533636c77a9047820a5b507
naji-alhusami <najihussami@gmail.com>
2023-12-20 14:09:00 +0300

    added navbar and and product categories

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React, { FC } from "react";
+
+interface NavItemsProps {}
+
+const NavItems: FC<NavItemsProps> = () => {
+  return <div></div>;
+};
+
+export default NavItems;
