7fe1e74890dbbb3fe5fb8305eda778cd9d43140e
naji-alhusami <najihussami@gmail.com>
2023-12-20 14:34:51 +0300

    finished the main elements of the navbar

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,11 +1,41 @@
 "use client";
 
-import React, { FC } from "react";
+import { PRODUCT_CATEGORIES } from "@/config";
+import React, { FC, useState } from "react";
+import NavItem from "./NavItem";
 
 interface NavItemsProps {}
 
 const NavItems: FC<NavItemsProps> = () => {
-  return <div></div>;
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  const isAnyOpen = activeIndex !== null;
+
+  return (
+    <div className="flex gap-4 h-full">
+      {PRODUCT_CATEGORIES.map((category, index) => {
+        const handleOpen = () => {
+          if (activeIndex === index) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(index);
+          }
+        };
+
+        const isOpen = index === activeIndex;
+
+        return (
+          <NavItem
+            key={category.value}
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavItems;
