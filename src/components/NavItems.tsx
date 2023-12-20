bb82b74b79c1ddb0a12d617ad5e74bf63fb1b2cb
naji-alhusami <najihussami@gmail.com>
2023-12-20 16:05:00 +0300

    Added custom hook for closing the navbar list

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,18 +1,37 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import React, { FC, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 interface NavItemsProps {}
 
 const NavItems: FC<NavItemsProps> = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
   const isAnyOpen = activeIndex !== null;
 
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, index) => {
         const handleOpen = () => {
           if (activeIndex === index) {
