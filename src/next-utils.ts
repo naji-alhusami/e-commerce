e3fabf3f5c94e0e6c5f4a751490f737b98a9c3fc
naji-alhusami <najihussami@gmail.com>
2023-12-22 16:21:11 +0300

    finished sending email functionality

--- src/next-utils.ts
+++ src/next-utils.ts
@@ -5,6 +5,6 @@ const PORT = Number(process.env.PORT) || 3000;
 export const nextApp = next({
   dev: process.env.NODE_ENV !== "production",
   port: PORT,
-});
+}); //This initializes a Next.js application using the next function. It takes an options object as an argument, where dev is set to true if the NODE_ENV environment variable is not set to "production", and port is set to the value of the PORT constant.
 
-export const nextHandler = nextApp.getRequestHandler();
+export const nextHandler = nextApp.getRequestHandler(); //This creates a request handler using getRequestHandler() method of the nextApp. The request handler is responsible for handling all incoming HTTP requests and serving the appropriate pages or assets based on the Next.js application's configuration.
--- src/trpc/auth-router.ts
+++ src/trpc/auth-router.ts
@@ -2,6 +2,7 @@ import { AuthCredentialsValidator } from "../lib/validators/account-credentials-
 import { publicProcedure, router } from "./trpc";
 import { getPayloadClient } from "../get-payload";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
 export const authRouter = router({
   createPayloadUser: publicProcedure // (any name: createPayloadUser) create user inside CMS, public to use it without signin
@@ -32,5 +33,25 @@ export const authRouter = router({
       });
 
       return { success: true, sentToEmail: email };
+    }),
+
+  verifyEmail: publicProcedure
+    .input(z.object({ token: z.string() }))
+    .query(async ({ input }) => { // we use query because we are not changing any data (just reading)
+      const { token } = input;
+
+      const payload = await getPayloadClient();
+
+      const isVerified = await payload.verifyEmail({
+        // verifyEmail is already inside payload (built-in)
+        collection: "users",
+        token,
+      });
+
+      if (!isVerified) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+      }
+
+      return { success: true }; // our CMS will after that automatically change the _verified in Mongo o true
     }),
 });
