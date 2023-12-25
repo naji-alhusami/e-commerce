2f8901cecafe1825d98eb470eb621c0a4cba0381
naji-alhusami <najihussami@gmail.com>
2023-12-25 16:41:43 +0300

    added items to cart finished

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -14,7 +14,10 @@ import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { formatPrice } from "@/lib/utils";
 import Image from "next/image";
-import { FC, ReactNode } from "react";
+import { useCart } from "@/hooks/use-cart";
+import CartItem from "./CartItem";
+import { ScrollArea } from "./ui/scroll-area";
+import { useEffect, useState } from "react";
 
 // interface CartProps {
 //     className:string,
@@ -22,7 +25,19 @@ import { FC, ReactNode } from "react";
 // }
 
 const Cart = ({}) => {
-  const itemCount = 0;
+  const { items } = useCart();
+  const itemCount = items.length;
+
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+
+  useEffect(() => { // because the server has 0 and client side has another storage
+    setIsMounted(true);
+  }, []);
+
+  const cartTotal = items.reduce(
+    (total, { product }) => total + product.price,
+    0
+  );
 
   const fee = 1;
 
@@ -34,16 +49,22 @@ const Cart = ({}) => {
           className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
         />
         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-          0
+          {isMounted ? itemCount : 0}
         </span>
       </SheetTrigger>
       <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
-          <SheetTitle>Cart (0)</SheetTitle>
+          <SheetTitle>Cart ({itemCount})</SheetTitle>
         </SheetHeader>
         {itemCount > 0 ? (
           <>
-            <div className="flex w-full flex-col pr-6">cart items</div>
+            <div className="flex w-full flex-col pr-6">
+              <ScrollArea>
+                {items.map(({ product }) => (
+                  <CartItem key={product.id} product={product} />
+                ))}
+              </ScrollArea>
+            </div>
             <div className="space-y-4 pr-6">
               <Separator />
               <div className="space-y-1.5 text-sm">
@@ -57,7 +78,7 @@ const Cart = ({}) => {
                 </div>
                 <div className="flex">
                   <span className="flex-1">Total</span>
-                  <span>{formatPrice(fee)}</span>
+                  <span>{formatPrice(cartTotal + fee)}</span>
                 </div>
               </div>
 
